72328ecba4f428f8b3e2863132406f27b63ae9b4
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-02-06 22:48:55 -0500

    Version 1.0.0 - Got weather info to display for first three hours of each day.

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
-//Version 0.2.1
+//Version 0.3.1
 
 let appId = "f53fbf01c28e747d1a37c1514dad5367";
+let days = {0:"Sunday", 1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"}
 
 /*Makes use of jQuery API, instead of fetch API*/ 
 function getKingstonWeather() {
     var result;
     $.get(`https://api.openweathermap.org/data/2.5/forecast?id=3489854&APPID=`+appId, function(data, status) {
         if(status == 'success') {
-            var weatherDescription= document.getElementById("KingstonDescription");
-            for(i = 1; i < 6; i++) {
-                let dayAndDate = document.getElementById(`KingstonDayAndDate${i}`);
-                let weatherCondition = document.getElementById(`weatherConditionKingston${i}`);
-                let weatherIcon = document.getElementById(`documentIconImgK${i}`);
-
-                weatherIcon.src = 'http://openweathermap.org/img/wn/'+data.list[0].weather[0].icon+'.png';
-
+            dateCheck = true;
+            var dateInQuestion;
+            var element = 1;
+            for(i = 0; i < (data.list).length; i++) {
+                currentDate = (data.list[i].dt_txt.split(" "))[0];
+                if(dateCheck == true){
+                    dateCheck = false;
+                    dateInQuestion = (data.list[i].dt_txt.split(" "))[0];
+                    var weatherDescription= document.getElementById(`weatherConditionKingston${element}`);
+                    dateTime = data.list[i].dt_txt.split(" ");
+                    dArr = (dateTime[0].split("-") +","+ dateTime[1].split(":")).split(",");
+                    date = new Date(parseInt(dArr[0],10),parseInt(dArr[1]-1,10),parseInt(dArr[2]),parseInt(dArr[3],10),parseInt(dArr[4],10),parseInt(dArr[5],10));
+                    let dayAndDate = document.getElementById(`KingstonDayAndDate${element}`);
+                    let weatherCondition = document.getElementById(`weatherConditionKingston${element}`);
+                    let weatherIcon = document.getElementById(`documentIconImgK${element}`);
+                    weatherIcon.src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`;
+                    dayAndDate.innerHTML =  date.toString();
+                    weatherDescription.innerHTML = data.list[i].weather[0].description;
+                } else if(dateInQuestion != currentDate){
+                    dateCheck = true;
+                    element += 1;
+                }
             }
-            
-            
-            
-            
-            //alert("Data: " + data + "\nStatus: " + status)
-            /*switch(data.list[0].weather[0].main){
-                case 'Clear':
-                    document.getElementById("KingstonContainer").style.backgroundImage = 'url("clearSky.jpg")';
-                    break;
-
-                case 'Clouds':
-                    document.getElementById("KingstonContainer").style.backgroundImage = 'url("cloudy.jpg")';
-                    break;
-
-                case 'Rain':
-                case 'Drizzle':
-                case 'Mist':
-                    document.getElementById("KingstonContainer").style.backgroundImage = 'url("rainy.jpg")';
-                    break;
-
-                case 'Thunderstorm':
-                    document.getElementById("KingstonContainer").style.backgroundImage = 'url("thunderstorm.jpg")';
-                    break;
-
-                default:
-                    break;*/
-            
         }
     })
 }
@@ -50,41 +38,33 @@ function getMontegoBayWeather() {
     var result;
     $.get(`https://api.openweathermap.org/data/2.5/forecast?id=3489460&APPID=`+appId, function(data, status) {
         if(status == 'success') {
-            var weatherDescription= document.getElementById("MontegoBayDescription");
-            for(i = 1; i < 6; i++) {
-                let dayAndDate = document.getElementById(`MontegoBayDayAndDate${i}`);
-                let weatherCondition = document.getElementById(`weatherConditionMontegoBay${i}`);
-                let weatherIcon = document.getElementById(`documentIconImgMB${i}`);
-
-                weatherIcon.src = 'http://openweathermap.org/img/wn/'+data.list[0].weather[0].icon+'.png';
+            dateCheck = true;
+            var dateInQuestion;
+            var element = 1;
+            for(i = 0; i < (data.list).length; i++) {
+                currentDate = (data.list[i].dt_txt.split(" "))[0];
+                if(dateCheck == true){
+                    dateCheck = false;
+                    dateInQuestion = (data.list[i].dt_txt.split(" "))[0];
+                    var weatherDescription= document.getElementById(`weatherConditionMontegoBay${element}`);
+                    dateTime = data.list[i].dt_txt.split(" ");
+                    dArr = (dateTime[0].split("-") +","+ dateTime[1].split(":")).split(",");
+                    date = new Date(parseInt(dArr[0],10),parseInt(dArr[1]-1,10),parseInt(dArr[2]),parseInt(dArr[3],10),parseInt(dArr[4],10),parseInt(dArr[5],10));
+                    let dayAndDate = document.getElementById(`MontegoBayDayAndDate${element}`);
+                    let weatherCondition = document.getElementById(`weatherConditionMontegoBay${element}`);
+                    let weatherIcon = document.getElementById(`documentIconImgMB${element}`);
+                    weatherIcon.src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`;
+                    dayAndDate.innerHTML =  date.toString();
+                    weatherDescription.innerHTML = data.list[i].weather[0].description;
+                } else if(dateInQuestion != currentDate){
+                    dateCheck = true;
+                    element += 1;
+                }
+                
             }
-            //alert("Data: " + data + "\nStatus: " + status)
-            /*switch(data.list[0].weather[0].main){
-                case 'Clear':
-                    document.getElementById("MontegoBayContainer").style.backgroundImage = 'url("clearSky.jpg")';
-                    break;
-
-                case 'Clouds':
-                    document.getElementById("MontegoBayContainer").style.backgroundImage = 'url("cloudy.jpg")';
-                    break;
-
-                case 'Rain':
-                case 'Drizzle':
-                case 'Mist':
-                    document.getElementById("MontegoBayContainer").style.backgroundImage = 'url("rainy.jpg")';
-                    break;
-
-                case 'Thunderstorm':
-                    document.getElementById("MontegoBayContainer").style.backgroundImage = 'url("thunderstorm.jpg")';
-                    break;
-
-                default:
-                    break;*/
-            
         }
     })
 }
-
 
 function init(){
     getKingstonWeather();
